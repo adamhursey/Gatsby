d47bbf781f58496abb2aa21f4b680990b65296be
Adam Hursey <adamhursey24@gmail.com>
2020-12-17 01:09:12 -0700

    finished video 13

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -1,7 +1,9 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
 
 export default {
+  // Computer Name
   name: 'pizza',
+  // visible title
   title: 'Pizzas',
   type: 'document',
   icon,
@@ -34,8 +36,32 @@ export default {
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
-      validation: (Rule) => Rule.min(1000),
-      // TODO: Add custom componenet
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
